0a124ad76660c0df75ec537a8b0d620b8d8233d2
PradeepArkasali <apradeep7@gmail.com>
2018-10-26 17:18:02 +0530

    Implementation of Singleton Pattern

--- SingletonPattern/Singleton.cs
+++ SingletonPattern/Singleton.cs
@@ -6,20 +6,34 @@ using System.Threading.Tasks;
 
 namespace SingletonPattern
 {
+    /// <summary>
+    /// SingleTon, class is marked sealed because it should not be inherited and can create new object.
+    /// </summary>
     public sealed class Singleton
     {
-        private static readonly Singleton instance = new Singleton();
+        private static Singleton instance;
         private int numberOfInstances = 0;
 
+        /// <summary>
+        /// The constructor is private because caller should not create the object with new operator
+        /// </summary>
         private Singleton()
         {
             this.numberOfInstances++;
         }
 
+        /// <summary>
+        /// Works well with the single thread implementation, but has draw back for multi-thread implementation fails because
+        /// if(instance == null), will be invoked by multiple threads, hence creating multiple objects.
+        /// </summary>
         public static Singleton Instance
         {
             get
             {
+                if(instance == null)
+                {
+                    instance = new Singleton();
+                }
                 return instance;
             }
         }
---
+++ SingletonPattern/SingletonWithDoubleCheckedLocking.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    /// <summary>
+    /// Singleton class with double check locking
+    /// </summary>
+    public sealed class SingletonWithDoubleCheckedLocking
+    {
+        /// <summary>
+        /// We are using volatile to ensure that assignment to the instance variable finishes before it’s access.
+        /// The volatile keyword indicates that a field might be modified by multiple threads that are executing at the same time.
+        /// Fields that are declared volatile are not subject to compiler optimizations that
+        ///assume access by a single thread.This ensures that the most up-todate
+        ///value is present in the field at all times.
+        ///In simple terms, the volatile keyword can help you to provide
+        ///a serialize access mechanism.In other words, all threads will
+        ///observe the changes by any other thread as per their execution order.
+        /// </summary>
+        public static volatile SingletonWithDoubleCheckedLocking instance;
+
+        private static object lockObject = new Object();
+
+        private SingletonWithDoubleCheckedLocking() { }
+
+        public static SingletonWithDoubleCheckedLocking Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (lockObject)
+                    {
+                        if(instance == null)
+                        {
+                            instance = new SingletonWithDoubleCheckedLocking();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+    }
+}
